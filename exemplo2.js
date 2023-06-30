9fa81bbff187766ff3dfb09378df8dca5b930f8f
felipe <felipe7.dantas@gmail.com>
2023-06-30 20:00:33 -0300

    atualização

--- exemplo2.js
+++ exemplo2.js
@@ -13,16 +13,28 @@ function mult(num1, num2) {
 
 function mediaNotas() {
   const valor1 = parseFloat(document.getElementById("valor1").value);
+  if(isNaN(valor1)){
+    document.getElementById("result").innerHTML = "Nota não informada";
+  }
   const valor2 = parseFloat(document.getElementById("valor2").value);
+  if(isNaN(valor2)){
+    document.getElementById("result").innerHTML = "Nota não informada";
+  }
   const media = (valor1 + valor2) / 2;
+  if(isNaN(media)){
+    document.getElementById("status").innerHTML = "Nota não informada";
+  }else{
   document.getElementById("result").innerHTML = media;
-
+  }
 // aprovado *se* media for maior ou igual a 6
 if (media >= 6) {
   document.getElementById("status").innerHTML = "APROVADO";
+} else if (media>=5) {
+  document.getElementById("status").innerHTML = "RECUPERAÇÃO";
 } else {
   document.getElementById("status").innerHTML = "REPROVADO";
 }
+
 }
 
 function limpar() {
